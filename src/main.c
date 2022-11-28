577e07f8e2ececef5883c934f8dd8a1941ea020b
Stephen Catsamas <seca8193@gmail.com>
2022-11-29 08:44:26 +1100

    got dppi working with led and button

--- src/main.c
+++ src/main.c
@@ -7,6 +7,10 @@
 
 
 #include <nrfx.h>
+#include <hal/nrf_timer.h>
+#include <hal/nrf_dppi.h>
+#include <hal/nrf_gpiote.h>
+#include <hal/nrf_gpio.h>
 #include <zephyr.h>
 #include <device.h>
 #include <drivers/gpio.h>
@@ -70,15 +74,87 @@ void main(void)
 	
 	gpio_pin_configure(gpio, SCCB_PEN, GPIO_OUTPUT);
 	gpio_pin_configure(gpio, SCCB_PDN, GPIO_OUTPUT);
-	gpio_pin_configure(gpio, 18, GPIO_OUTPUT);
 	gpio_pin_set_raw(gpio, SCCB_PEN, 1);
 	gpio_pin_set_raw(gpio, SCCB_PDN, 0);
 		
+		
+	NRF_P0->PIN_CNF[18] = 0b00000000000000000000000000000001; 
+	nrf_gpio_cfg_input(21,NRF_GPIO_PIN_PULLUP);
+
+	// nrf_timer_frequency_set(NRF_TIMER0, NRF_TIMER_FREQ_125kHz);
+	// nrf_timer_mode_set(NRF_TIMER0, NRF_TIMER_MODE_TIMER);
+	// nrf_timer_cc_set(NRF_TIMER0,NRF_TIMER_CC_CHANNEL0, 0x10);
+	// nrf_timer_bit_width_set(NRF_TIMER0, NRF_TIMER_BIT_WIDTH_8);
+	// nrf_timer_shorts_set(NRF_TIMER0, 0b00000000000000000000000000000001);//reset timer on match with compare 0
+	// nrf_timer_publish_set(NRF_TIMER0, NRF_TIMER_EVENT_COMPARE0,0);
+	// nrf_gpiote_subscribe_set(NRF_GPIOTE, NRF_GPIOTE_TASK_OUT_0 ,0);
+	// nrf_gpiote_task_configure(NRF_GPIOTE,
+                                                 // 0,
+                                                 // 18,
+                                                 // NRF_GPIOTE_POLARITY_TOGGLE,
+                                                 // NRF_GPIOTE_INITIAL_VALUE_HIGH);
+	// nrf_gpiote_task_enable(NRF_GPIOTE,0);
+	// nrf_dppi_task_trigger(NRF_DPPIC, NRF_DPPI_TASK_CHG0_EN);
+	// nrf_timer_task_trigger(NRF_TIMER0,NRF_TIMER_TASK_START);
+	
+	// uint8_t chen = nrf_dppi_channel_check(NRF_DPPIC, 0);
+	// printk("print %i", chen);
+	
+	
+	/* Configure GPIOTE Index 0 to be an Event */
+    nrf_gpiote_event_configure(NRF_GPIOTE,0,21,NRF_GPIOTE_POLARITY_HITOLO);
+
+    /* Configure GPIOTE Index 1 to be a Task*/
+    nrf_gpiote_task_configure(NRF_GPIOTE,1,18,NRF_GPIOTE_POLARITY_TOGGLE,NRF_GPIOTE_INITIAL_VALUE_LOW);
+
+    /* Index 0 will Publish on DPPI Channel 0 */
+    nrf_gpiote_publish_set(NRF_GPIOTE,NRF_GPIOTE_EVENT_IN_0,0);
+
+    /* Index 1 will Subscribe on DPPI Channel 0 */
+    nrf_gpiote_subscribe_set(NRF_GPIOTE,NRF_GPIOTE_TASK_OUT_1,0);
+
+    /* Enable Publish and Subscribe */
+    nrf_gpiote_event_enable(NRF_GPIOTE,0);
+    nrf_gpiote_task_enable(NRF_GPIOTE,1);
+
+    /* Enable DPPI Channel */
+    nrf_dppi_channels_enable(NRF_DPPIC, 1);
+
+    /* Validate things did get enabled */
+    printk("[0] is %d\n",nrf_gpiote_te_is_enabled(NRF_GPIOTE,0));
+    printk("[1] is %d\n",nrf_gpiote_te_is_enabled(NRF_GPIOTE,1));
+    printk("[2] is %d\n",nrf_dppi_channel_check(NRF_DPPIC,0));
+	
 	while(1){
-		// NRF_GPIO->OUT |= (0x01 << 18);
-		// NRF_GPIO->OUT &= ~(0x01 << 18);
-		NRF_P0->OUT ^= (0x01 << 18);
+		
 	}
+	
+	
+	
+	// while(1){
+		// // NRF_GPIO->OUT |= (0x01 << 18);
+		// // NRF_GPIO->OUT &= ~(0x01 << 18);
+		// NRF_P0->OUTSET = (0x01 << 18);
+		// NRF_P0->OUTCLR = (0x01 << 18);
+		// NRF_P0->OUTSET = (0x01 << 18);
+		// NRF_P0->OUTCLR = (0x01 << 18);
+		// NRF_P0->OUTSET = (0x01 << 18);
+		// NRF_P0->OUTCLR = (0x01 << 18);
+		// NRF_P0->OUTSET = (0x01 << 18);
+		// NRF_P0->OUTCLR = (0x01 << 18);
+		// NRF_P0->OUTSET = (0x01 << 18);
+		// NRF_P0->OUTCLR = (0x01 << 18);
+		// NRF_P0->OUTSET = (0x01 << 18);
+		// NRF_P0->OUTCLR = (0x01 << 18);
+		// NRF_P0->OUTSET = (0x01 << 18);
+		// NRF_P0->OUTCLR = (0x01 << 18);
+		// NRF_P0->OUTSET = (0x01 << 18);
+		// NRF_P0->OUTCLR = (0x01 << 18);
+		// NRF_P0->OUTSET = (0x01 << 18);
+		// NRF_P0->OUTCLR = (0x01 << 18);
+		// NRF_P0->OUTSET = (0x01 << 18);
+		// NRF_P0->OUTCLR = (0x01 << 18);
+	// }
 	
 	i2c_sccb = device_get_binding(DT_LABEL(DT_NODELABEL(i2c2)));
 	printk("bind %s\n", i2c_sccb->name);
