dd3e43636ab87ae67ac499b236e1ed5b09588779
Stephen Catsamas <seca8193@gmail.com>
2022-12-06 14:17:32 +1100

    working sd card reading and board configuration

--- src/main.c
+++ src/main.c
@@ -13,10 +13,14 @@
 #include <hal/nrf_gpio.h>
 #include <zephyr.h>
 #include <device.h>
+#include <storage/disk_access.h>
+#include <fs/fs.h>
+#include <ff.h>
 #include <drivers/gpio.h>
 #include <sys/util.h>
 #include <sys/printk.h>
 #include <inttypes.h>
+#include <logging/log.h>
 
 #include <drivers/i2c.h>
 
@@ -49,6 +53,9 @@
 #if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
 #error "Unsupported board: sw0 devicetree alias is not defined"
 #endif
+
+LOG_MODULE_REGISTER(main);
+
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
 							      {0});
 static struct gpio_callback button_cb_data;
@@ -65,6 +72,19 @@ static const struct device * gpio;
 const struct device * i2c_sccb;
 
 uint8_t imbuf[640*256];
+
+static FATFS fat_fs;
+/* mounting info */
+static struct fs_mount_t mp = {
+	.type = FS_FATFS,
+	.fs_data = &fat_fs,
+};
+
+/*
+*  Note the fatfs library is able to mount only strings inside _VOLUME_STRS
+*  in ffconf.h
+*/
+static const char *disk_mount_pt = "/SD:";
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
 		    uint32_t pins)
@@ -154,9 +174,96 @@ void get_frame(void){
 	}
 }
 
+
+
+static int lsdir(const char *path)
+{
+	int res;
+	struct fs_dir_t dirp;
+	static struct fs_dirent entry;
+
+	fs_dir_t_init(&dirp);
+
+	/* Verify fs_opendir() */
+	res = fs_opendir(&dirp, path);
+	if (res) {
+		printk("Error opening dir %s [%d]\n", path, res);
+		return res;
+	}
+
+	printk("\nListing dir %s ...\n", path);
+	for (;;) {
+		/* Verify fs_readdir() */
+		res = fs_readdir(&dirp, &entry);
+
+		/* entry.name[0] == 0 means end-of-dir */
+		if (res || entry.name[0] == 0) {
+			break;
+		}
+
+		if (entry.type == FS_DIR_ENTRY_DIR) {
+			printk("[DIR ] %s\n", entry.name);
+		} else {
+			printk("[FILE] %s (size = %zu)\n",
+				entry.name, entry.size);
+		}
+	}
+
+	/* Verify fs_closedir() */
+	fs_closedir(&dirp);
+
+	return res;
+}
+
 void main(void)
 {
 	int ret;
+		
+	printk("begin!\n");
+	/* raw disk i/o */
+	do {
+		static const char *disk_pdrv = "SD";
+		uint64_t memory_size_mb;
+		uint32_t block_count;
+		uint32_t block_size;
+
+		if (disk_access_init(disk_pdrv) != 0) {
+			LOG_ERR("Storage init ERROR!");
+			break;
+		}
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_COUNT, &block_count)) {
+			LOG_ERR("Unable to get sector count");
+			break;
+		}
+		LOG_INF("Block count %u", block_count);
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_SIZE, &block_size)) {
+			LOG_ERR("Unable to get sector size");
+			break;
+		}
+		printk("Sector size %u\n", block_size);
+
+		memory_size_mb = (uint64_t)block_count * block_size;
+		printk("Memory Size(MB) %u\n", (uint32_t)memory_size_mb>>20);
+	} while (0);
+
+	mp.mnt_point = disk_mount_pt;
+
+	int res = fs_mount(&mp);
+
+	if (res == FR_OK) {
+		printk("Disk mounted.\n");
+		lsdir(disk_mount_pt);
+	} else {
+		printk("Error mounting disk.\n");
+	}
+
+	while (1) {
+		k_sleep(K_MSEC(1000));
+	}
 	
 	
 	sccb_setup();
