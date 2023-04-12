6eacad2f74030975182d42e20632ef044f01e4cf
Stephen Catsamas <seca8193@gmail.com>
2023-04-12 16:21:28 +1000

    Revert "-fixed byteorder issue by adding delay after ov7675 reset"

--- src/main.c
+++ src/main.c
@@ -215,7 +215,7 @@ int loop(void){
 	update_status();
 	
 	LOG_INF("ov7675 initialisation");
-	ov7675_init(1000);
+	ov7675_init(mcfg.im_cfg.auto_range_time);
 
 	LOG_INF("ov7675 capture");
 	ov7675_capture(capture.data);
@@ -223,36 +223,36 @@ int loop(void){
 	LOG_INF("image -> sdhc");
 	sdhc_write_image(mcfg.sd_cfg.image_path, 
 					 &capture);
-	// sdhc_write_status(mcfg.sd_cfg.status_path, 
-					 // &stats);
+	sdhc_write_status(mcfg.sd_cfg.status_path, 
+					 &stats);
 					 
-	// if ((d > 0) && (0 == (stats.captures % d))){
-		// LOG_INF("image -> ftp");
-		// ftp_write_image(&mcfg.ftp_cfg, 
-						// &capture);
-		// ftp_write_status(&mcfg.ftp_cfg, 
-					 // &stats);
-	// }
-	// LOG_INF("done");
+	if ((d > 0) && (0 == (stats.captures % d))){
+		LOG_INF("image -> ftp");
+		ftp_write_image(&mcfg.ftp_cfg, 
+						&capture);
+		ftp_write_status(&mcfg.ftp_cfg, 
+					 &stats);
+	}
+	LOG_INF("done");
 	
-	// switch (mcfg.trig_cfg.trig_type){
-	// case TIME_TIRGGER:
-		// LOG_INF("time sleep");
-		// sleepy(mcfg.trig_cfg.logging_interval);
-		// break;		
-	// case UART_TRIGGER:
-		// LOG_INF("uart sleep");
-		// // uart_sei();
-		// sleepy(0);
-		// // uart_cli();
-		// break;
-	// default:
-		// LOG_ERR("trig_type misconfigred, oops!");
-		// k_oops();
-		// break;	
-	// }
+	switch (mcfg.trig_cfg.trig_type){
+	case TIME_TIRGGER:
+		LOG_INF("time sleep");
+		sleepy(mcfg.trig_cfg.logging_interval);
+		break;		
+	case UART_TRIGGER:
+		LOG_INF("uart sleep");
+		// uart_sei();
+		sleepy(0);
+		// uart_cli();
+		break;
+	default:
+		LOG_ERR("trig_type misconfigred, oops!");
+		k_oops();
+		break;	
+	}
 	
-	// stats.captures++;
+	stats.captures++;
 	
 	return 0;
 }
@@ -268,12 +268,12 @@ void main(void){
 	
 	LOG_INF("begin!");
 	
-	// ret = setup();
-	// if(ret < 0){
-		// k_oops();
-		// //lockup program and halt
-		// //try call for help
-	// }
+	ret = setup();
+	if(ret < 0){
+		k_oops();
+		//lockup program and halt
+		//try call for help
+	}
 	
 		
 	while(1){
